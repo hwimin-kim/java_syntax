72fdf2d6a1d890898ddaab1395f1a8c19882b8d4
hwimin-kim <gnlals7841@gmail.com>
2022-03-31 17:16:20 +0900

    계산기 예제 추가

---
+++ project/src/org/opentutorials/javatutorials/object/Calculator2Demo.java
@@ -0,0 +1,15 @@
+package org.opentutorials.javatutorials.object;
+
+public class Calculator2Demo {
+	
+	public static void sum(int left, int right) {
+		System.out.println(left + right);
+	}
+
+	public static void main(String[] args) {
+		sum(10, 20);
+		sum(20, 40);
+
+	}
+
+}
