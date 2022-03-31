ab8e68d03b217db0fc9628482bf8fd474123b971
hwimin-kim <gnlals7841@gmail.com>
2022-03-31 17:30:17 +0900

    char string number 연산 예제 추가

---
+++ project/src/org/opentutorials/javatutorials/numberstring/CharString.java
@@ -0,0 +1,16 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class CharString {
+
+	public static void main(String[] args) {
+
+		System.out.println('생');
+		System.out.println("생활코딩");
+		//System.out.println('생활코딩');
+		System.out.println("동서" + "대");
+		
+		System.out.println("abcd \"12345\"");
+		System.out.println("HTML\nCSS\nJavaScript\n");
+	}
+
+}
